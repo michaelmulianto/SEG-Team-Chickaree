ba6d7bb89ceaad5999c6811f6aac1ed9d61d29e4
Kieran Woolley <kieran.woolley@kcl.ac.uk>
2021-11-14 14:34:14 +0000

    Create sign up form, and home and sign up templates

---
+++ clubs/forms.py
@@ -0,0 +1,58 @@
+"""
+Define the forms for the system.
+
+Currently implemented forms:
+    - SignUpForm
+"""
+
+from django import forms
+from clubs.models import User
+from django.core.validators import RegexValidator
+
+class SignUpForm(forms.ModelForm):
+    """Form to allow a visitor to make an account and become a registered user"""
+    class Meta:
+        model = User
+        # If we do not specify fields, it will use all of them.
+        # We don't want to include password as we use 2 fields
+        fields = ['first_name', 'last_name', 'username', 'email']
+
+    # Define fields excluded in Meta
+    new_password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput,
+        validators=[RegexValidator(
+            regex=r'[A-Z]',
+            message='Password must contain an uppercase character.'
+            ),
+            RegexValidator(
+            regex=r'[a-z]',
+            message='Password must contain a lowercase character.'
+            ),
+            RegexValidator(
+            regex=r'[0-9]',
+            message='Password must contain a number.'
+            ),
+            ]
+        )
+    password_confirmation = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+
+    # Verify our custom fields are valid.
+    def clean(self):
+        super().clean()
+        new_password = self.cleaned_data.get('new_password')
+        password_confirmation = self.cleaned_data.get('password_confirmation')
+        if new_password != password_confirmation:
+            self.add_error('password_confirmation', 'Confirmation does not match password.')
+
+    # Create new user using form data.
+    def save(self):
+        super().save(commit=False)
+        user = User.objects.create_user(
+            self.cleaned_data.get('username'),
+            first_name = self.cleaned_data.get('first_name'),
+            last_name = self.cleaned_data.get('last_name'),
+            email = self.cleaned_data.get('email'),
+            password = self.cleaned_data.get('new_password'),
+        )
+        return user
--- system/urls.py
+++ system/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from clubs import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('sign_up/', views.sign_up, name='sign_up'),
 ]
