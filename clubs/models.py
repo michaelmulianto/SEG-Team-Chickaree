cb08f6c0a4c9a04d0d1f6ff4f89e5c16f2c09acd
Kieran Woolley <kieran.woolley@kcl.ac.uk>
2021-11-14 11:27:09 +0000

    Add user model

--- clubs/models.py
+++ clubs/models.py
@@ -1,3 +1,33 @@
-from django.db import models
+"""
+Define the models for the system.
 
-# Create your models here.
+Currently implemented models:
+    - User
+"""
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    """Model for a generic user, independent of any clubs"""
+    username = models.CharField(
+    max_length=32,
+    unique=True,
+    blank=False,
+    )
+
+    last_name = models.CharField(
+    max_length=48,
+    unique=False,
+    blank=False
+    )
+
+    first_name = models.CharField(
+    max_length=48,
+    unique=False,
+    blank=False,
+    )
+
+    email = models.EmailField(
+    unique=True,
+    blank=False,
+    )
