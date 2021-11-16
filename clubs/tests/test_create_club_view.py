29b4d517246a01132f5e1309d08e559cbb1a7b14
Kieran Woolley <kieran.woolley1@gmail.com>
2021-11-16 14:59:13 +0000

    Validate GET requests to create_club

--- clubs/tests/test_create_club_view.py
+++ clubs/tests/test_create_club_view.py
@@ -1,4 +1,4 @@
-# """Test user-facing implementation of the create club form."""
+"""Test user-facing implementation of the create club form."""
 
 from django.test import TestCase
 from django.urls import reverse
@@ -27,13 +27,13 @@ class CreateClubViewTest(TestCase):
     def test_create_club_url(self):
         self.assertEqual(self.url, '/create_club/')
 
-    def test_get_create_club_is_forbidden(self):
+    def test_get_create_club_loads_empty_form(self):
         self.client.login(username=self.user.username, password="Password123")
         user_count_before = Club.objects.count()
         response = self.client.get(self.url, follow=True)
         user_count_after = Club.objects.count()
         self.assertEqual(user_count_after, user_count_before)
-        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.status_code, 200)
 
     def test_create_club_redirects_when_not_logged_in(self):
         user_count_before = Club.objects.count()
@@ -45,21 +45,21 @@ class CreateClubViewTest(TestCase):
         user_count_after = Club.objects.count()
         self.assertEqual(user_count_after, user_count_before)
 
-    # def test_successful_create_club(self):
-    #     self.client.login(username=self.user.username, password="Password123")
-    #     user_count_before = Club.objects.count()
-    #     response = self.client.post(self.url, self.data, follow=True)
-    #     user_count_after = Club.objects.count()
-    #     self.assertEqual(user_count_after, user_count_before+1)
-    #     new_club = Club.objects.latest('created_on')
-    #     #self.assertEqual(self.user, new_post.author)
-    #     response_url = reverse('create_club')
-    #     self.assertRedirects(
-    #         response, response_url,
-    #         status_code=302, target_status_code=200,
-    #         fetch_redirect_response=True
-    #     )
-    #     self.assertTemplateUsed(response, 'create_club.html')
+    def test_successful_create_club(self):
+        self.client.login(username=self.user.username, password="Password123")
+        user_count_before = Club.objects.count()
+        response = self.client.post(self.url, self.data, follow=True)
+        user_count_after = Club.objects.count()
+        self.assertEqual(user_count_after, user_count_before+1)
+        new_club = Club.objects.latest('created_on')
+        #self.assertEqual(self.user, new_post.author)
+        response_url = reverse('create_club')
+        self.assertRedirects(
+            response, response_url,
+            status_code=302, target_status_code=200,
+            fetch_redirect_response=True
+        )
+        self.assertTemplateUsed(response, 'create_club.html')
 
     def test_unsuccessful_create_club(self):
         self.client.login(username='@johndoe', password='Password123')
