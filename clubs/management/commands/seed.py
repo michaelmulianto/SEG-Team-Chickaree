4cba1df080ac3cbc4360bec8ec39d1da371c9bad
Michael Mulianto <michaelmulianto@gmail.com>
2021-11-18 15:13:26 +0000

    make unseed file

--- clubs/management/commands/seed.py
+++ clubs/management/commands/seed.py
@@ -1,7 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
+from faker import Faker
+from clubs.models import User, Club
 
 class Command(BaseCommand):
     """The database seeder."""
 
     def handle(self, *args, **options):
-        print("TODO: The database seeder will be added here...")
+        print("TODO: The databaseseeder will be added here...")
--- clubs/management/commands/unseed.py
+++ clubs/management/commands/unseed.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
+from microblogs.models import User
 
 class Command(BaseCommand):
-        """The database unseeder."""
-
-        def handle(self, *args, **options):
-            print("TODO: The database unseeder will be added here...")
+    def handle(self, *args, **options):
+        User.objects.filter(is_staff=False, is_superuser=False).delete()
+        Club.objects.delete()
