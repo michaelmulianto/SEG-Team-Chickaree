5ba7a0cfbe4921371eaae74b728cf4013259ee50
Liam Clark Gutierrez <liam.clark_gutierrez@kcl.ac.uk>
2021-11-19 01:13:19 +0000

    Add log out view and tests

--- clubs/views.py
+++ clubs/views.py
@@ -98,7 +98,7 @@ def apply_to_club(request, club_id):
     # It should not be possible for an invalid id to be passed to this point.
     if request.user.is_authenticated:
         desired_club = Club.objects.get(id = club_id)
-        current_user = request.user 
+        current_user = request.user
         # Ensure that the user does not have an existing application or membership to the club.
         if not(Application.objects.filter(club=desired_club, user = current_user).exists()) and not(Member.objects.filter(club=desired_club, user = current_user).exists()):
             Application.objects.create(
@@ -116,3 +116,7 @@ def show_club(request, club_id):
         return redirect('show_clubs')
     else:
         return render(request, 'show_club.html', {'club': club})
+
+def log_out(request):
+    logout(request)
+    return redirect('home')
