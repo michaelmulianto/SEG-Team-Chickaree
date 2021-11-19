98cbf876465dc69b36ee79bddcd214a1c23ff0bb
John Kelly Tamargo <john.kelly.tamargo@gmail.com>
2021-11-19 19:24:24 +0000

    Make sure entering correct values for the form redirects the user and updates their information

--- clubs/tests/test_edit_account_view.py
+++ clubs/tests/test_edit_account_view.py
@@ -11,9 +11,9 @@ class EditAccountViewTest(TestCase):
     def setUp(self):
         self.url = reverse('edit_account')
         self.form_input = {
-            'username' : 'johndoe2',
-            'first_name': 'John2',
-            'last_name': 'Doe2',
+            'username' : 'johndoeeee',
+            'first_name': 'Johnnnn',
+            'last_name': 'Doeeee',
             'email' : 'johndoe2@example.org',
         }
         self.user = User.objects.create_user(
@@ -55,6 +55,25 @@ class EditAccountViewTest(TestCase):
         self.assertEqual(self.user.last_name, 'Doe')
         self.assertEqual(self.user.email, 'johndoe@example.org')
 
+    def test_unsuccessful_edit_account_update_due_to_duplicate_username(self):
+        self.client.login(username=self.user.username, password='Password123')
+        second_user = self._create_second_user()
+        self.form_input['username'] = second_user.username
+        before_count = User.objects.count()
+        response = self.client.post(self.url, self.form_input)
+        after_count = User.objects.count()
+        self.assertEqual(after_count, before_count)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'edit_account.html')
+        form = response.context['form']
+        self.assertTrue(isinstance(form, EditAccountForm))
+        self.assertTrue(form.is_bound)
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.username, '@johndoe')
+        self.assertEqual(self.user.first_name, 'John')
+        self.assertEqual(self.user.last_name, 'Doe')
+        self.assertEqual(self.user.email, 'johndoe@example.org')
+
     def test_unsuccessful_edit_account_update_due_to_duplicate_email(self):
         self.client.login(username=self.user.username, password='Password123')
         second_user = self._create_second_user()
@@ -73,6 +92,24 @@ class EditAccountViewTest(TestCase):
         self.assertEqual(self.user.first_name, 'John')
         self.assertEqual(self.user.last_name, 'Doe')
         self.assertEqual(self.user.email, 'johndoe@example.org')
+
+    def test_succesful_profile_update(self):
+        self.client.login(username=self.user.username, password='Password123')
+        before_count = User.objects.count()
+        response = self.client.post(self.url, self.form_input, follow=True)
+        after_count = User.objects.count()
+        self.assertEqual(after_count, before_count)
+        response_url = reverse('account')
+        self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
+        self.assertTemplateUsed(response, 'account.html')
+        messages_list = list(response.context['messages'])
+        self.assertEqual(len(messages_list), 1)
+        self.assertEqual(messages_list[0].level, messages.SUCCESS)
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.username, 'johndoeeee')
+        self.assertEqual(self.user.first_name, 'Johnnnn')
+        self.assertEqual(self.user.last_name, 'Doeeee')
+        self.assertEqual(self.user.email, 'johndoe2@example.org')
 
 
     def _create_second_user(self):
