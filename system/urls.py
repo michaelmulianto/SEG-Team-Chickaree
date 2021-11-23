3e2077ca2cd1e25344b41402a39a2deb19b4735c
Michael Mulianto <michaelmulianto@gmail.com>
2021-11-23 14:59:25 +0000

    show club detail page

--- system/urls.py
+++ system/urls.py
@@ -29,6 +29,6 @@ urlpatterns = [
     path('change_password/', views.change_password, name = 'change_password'),
     path('clubs/', views.show_clubs, name = 'show_clubs'),
     path('apply_to_club/<int:club_id>', views.apply_to_club, name = 'apply_to_club'),
-    path('club_details/', views.club_details, name = 'club_details'),
+    path('club_details/<int:club_id>', views.club_details, name = 'club_details'),
     path('club/<int:club_id>', views.show_club, name='show_club'),
 ]
