e41645296b1fd97c1136f7541c753ed3df6b5976
Kieran Woolley <kieran.woolley1@gmail.com>
2021-11-25 19:17:49 +0000

    Ensure view uses helper

--- clubs/views.py
+++ clubs/views.py
@@ -119,26 +119,24 @@ def create_club(request):
     else:
         return render(request, 'create_club.html', {'form': CreateClubForm()})
 
+@login_required
 def apply_to_club(request, club_id):
     if request.method == 'POST':
-        if request.user.is_authenticated:
-            desired_club = Club.objects.get(id = club_id)
-            current_user = request.user
-            form = forms.ApplyToClubForm(request.POST)
-            if form.is_valid():
-                if not(Application.objects.filter(club=desired_club, user = current_user).exists()) and not(Member.objects.filter(club=desired_club, user = current_user).exists()):
-                    application = Application.objects.create(
-                        club = desired_club,
-                        user = current_user,
-                        experience = form.cleaned_data.get('experience'),
-                        personalStatement = form.cleaned_data.get('personalStatement'),
-                    )
-                    return redirect('show_clubs')
-            # Next line executes if one of the last 2 conditionals fail.
-            return render(request, 'apply_to_club.html', {'form': form, 'club':desired_club})
-        else:
-            return redirect('log_in')
-    else:
+        desired_club = Club.objects.get(id = club_id)
+        current_user = request.user
+         form = forms.ApplyToClubForm(request.POST)
+         if form.is_valid():
+            if not(Application.objects.filter(club=desired_club, user = current_user).exists()) and not(Member.objects.filter(club=desired_club, user = current_user).exists()):
+                application = Application.objects.create(
+                    club = desired_club,
+                    user = current_user,
+                    experience = form.cleaned_data.get('experience'),
+                    personalStatement = form.cleaned_data.get('personalStatement'),
+                )
+            return redirect('show_clubs')
+        # Invalid form
+        return render(request, 'apply_to_club.html', {'form': form, 'club':desired_club})
+    else: # GET
         return render(request, 'apply_to_club.html', {'form': forms.ApplyToClubForm(), 'club':Club.objects.get(id = club_id)})
 
 def show_clubs(request):
