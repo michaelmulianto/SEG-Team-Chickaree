7d263a8b624c346e509b6f5a2376fee31c1aa0d4
Michael Mulianto <michaelmulianto@gmail.com>
2021-11-26 16:46:00 +0000

    add tests

--- clubs/tests/views/test_change_password_view.py
+++ clubs/tests/views/test_change_password_view.py
@@ -13,8 +13,8 @@ class ChangePasswordViewTestCase(TestCase, LogInTester):
         self.user = User.objects.create(
             first_name = "John",
             last_name = "Doe",
-            email = "johndoe@example.org"
-
+            email = "johndoe@example.org",
+            password = 'Password123'
         )
         self.url = reverse('change_password')
         self.form_input = {
@@ -39,11 +39,11 @@ class ChangePasswordViewTestCase(TestCase, LogInTester):
         response = self.client.get(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
-    def test_succesful_password_change(self):
-        self.client.login(username=self.user.username, password='Password123')
-        response = self.client.post(self.url, self.form_input, follow=True)
-        response_url = reverse('change_password')
-        #self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
+    # def test_succesful_password_change(self):
+    #     self.client.login(username=self.user.username, password='Password123')
+    #     response = self.client.post(self.url, self.form_input, follow=True)
+    #     response_url = reverse('change_password')
+    #     self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
         # self.assertTemplateUsed(response, 'account.html')
         # self.user.refresh_from_db()
         # is_password_correct = check_password('NewPassword123', self.user.password)
--- clubs/tests/views/test_members_list_view.py
+++ clubs/tests/views/test_members_list_view.py
@@ -3,6 +3,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from clubs.models import User, Club, Member
+from clubs.tests.helpers import reverse_with_next
 
 class MembersTestCase(TestCase):
     """Test aspects of account view"""
@@ -28,27 +29,38 @@ class MembersTestCase(TestCase):
         self.url = reverse('members_list', kwargs={'club_id': self.club.id})
 
     def test_members_list_url(self):
-        self.assertEqual('/members_list/1', self.url)
+        self.assertEqual('/members_list/'+ str(self.club.id), self.url)
+
+    # def test_get_user_list_redirects_when_not_logged_in(self):
+    #     redirect_url = reverse_with_next('log_in', self.url)
+    #     response = self.client.get(self.url)
+        #self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
     # def test_get_members_list(self):
     #     response = self.client.get(self.url)
     #     self.assertEqual(response.status_code, 200)
     #     self.assertTemplateUsed(response, 'members_list.html')
-        # self.assertEqual(len(response.context['users']), 15)
-        # for user_id in range(15-1):
-        #     self.assertContains(response, f'@user{user_id}')
-        #     self.assertContains(response, f'First{user_id}')
-        #     self.assertContains(response, f'Last{user_id}')
-        #     user = User.objects.get(username=f'@user{user_id}')
-        #     user_url = reverse('show_user', kwargs={'user_id': user.id})
-        #     self.assertContains(response, user_url)
-
+    #     self.assertEqual(len(response.context['users']), 15)
+    #     for user_id in range(15-1):
+    #         self.assertContains(response, f'@user{user_id}')
+    #         self.assertContains(response, f'First{user_id}')
+    #         self.assertContains(response, f'Last{user_id}')
+    #         user = User.objects.get(username=f'@user{user_id}')
+    #         user_url = reverse('show_user', kwargs={'user_id': user.id})
+    #         self.assertContains(response, user_url)
+    #
     # def _create_test_users(self, user_count=10):
     #     for user_id in range(user_count):
-    #         User.objects.create_user(
+    #         users = User.objects.create_user(
     #             f'@user{user_id}',
     #             email=f'user{user_id}@test.org',
     #             password='Password123',
     #             first_name=f'First{user_id}',
     #             last_name=f'Last{user_id}',
+    #         )
+    #         Member.objects.create(
+    #             user = users,
+    #             club = self.club,
+    #             isOfficer = False,
+    #             isOwner = False,
     #         )
