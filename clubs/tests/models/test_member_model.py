5c1f7d4b0de6a447f4249fadc2430ba5067e75f2
Kieran Woolley <kieran.woolley1@gmail.com>
2021-11-28 17:12:07 +0000

    Add tests for user and club fields in member model

--- clubs/tests/models/test_member_model.py
+++ clubs/tests/models/test_member_model.py
@@ -23,9 +23,23 @@ class MemberModelTestCase(TestCase):
     def test_valid_member_object(self):
         self._assert_member_is_valid()
 
+    # Club field tests
     def test_club_field_cannot_be_blank(self):
         self.membership.club = None
         self._assert_member_is_invalid()
+
+    def test_club_field_cannot_contain_non_club_object(self):
+        with self.assertRaises(ValueError):
+            self.membership.club = self.user
+
+    # User field tests
+    def test_user_field_cannot_be_blank(self):
+        self.membership.user = None
+        self._assert_member_is_invalid()
+
+    def test_user_field_cannot_contain_non_user_object(self):
+        with self.assertRaises(ValueError):
+            self.membership.user = self.club
 
     #assertions
     def _assert_member_is_valid(self):
