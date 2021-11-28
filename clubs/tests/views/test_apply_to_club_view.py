646e5ea31dd76c0c34779dfd4aba4482ff94ed61
Liam Clark Gutierrez <liam.clark_gutierrez@kcl.ac.uk>
2021-11-28 05:59:59 +0000

    Add extra test cases for apply after already applying and already member

--- clubs/tests/views/test_apply_to_club_view.py
+++ clubs/tests/views/test_apply_to_club_view.py
@@ -3,8 +3,8 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.hashers import check_password
-from clubs.models import User, Club, Application
-from clubs.forms import CreateClubForm
+from clubs.models import User, Club, Application, Member
+from clubs.forms import ApplyToClubForm
 from clubs.tests.helpers import reverse_with_next
 
 class ApplyToClubViewTestCase(TestCase):
@@ -38,21 +38,46 @@ class ApplyToClubViewTestCase(TestCase):
         app_count_after = Application.objects.count()
         self.assertEqual(app_count_after, app_count_before)
 
-    def test_successful_application_to_club(self):
+    def test_unsuccessful_application_when_already_applied(self):
         self.client.login(username=self.user.username, password="Password123")
-        app_count_before = Application.objects.count()
-        response = self.client.post(self.url, self.data, follow=True)
-        app_count_after = Application.objects.count()
-        self.assertEqual(app_count_after, app_count_before+1)
-
-        # Should redirect user somewhere appropriate, indicating success.
-        response_url = reverse('show_clubs')
-        self.assertRedirects(
-            response, response_url,
-            status_code=302, target_status_code=200,
-            fetch_redirect_response=True
+        self.application = Application.objects.create(
+            club = self.club,
+            user = self.user,
+            experience = 2,
+            personal_statement = 'I love chess!'
         )
-        self.assertTemplateUsed(response, 'show_clubs.html')
+
+        app_count_before = Application.objects.count()
+        response = self.client.post(self.url, self.data)
+        app_count_after = Application.objects.count()
+        self.assertEqual(app_count_after, app_count_before)
+
+        self.assertEqual(response.status_code, 200) #OK
+        self.assertTemplateUsed(response, 'apply_to_club.html')
+        form = response.context['form']
+        self.assertTrue(isinstance(form, ApplyToClubForm))
+        self.assertTrue(form.is_bound)
+
+
+    def test_unsuccessful_application_when_already_member(self):
+        self.client.login(username=self.user.username, password="Password123")
+
+        self.membership = Member.objects.create(
+            club = self.club,
+            user = self.user,
+            is_owner = False
+        )
+
+        app_count_before = Application.objects.count()
+        response = self.client.post(self.url, self.data)
+        app_count_after = Application.objects.count()
+        self.assertEqual(app_count_after, app_count_before)
+
+        self.assertEqual(response.status_code, 200) #OK
+        self.assertTemplateUsed(response, 'apply_to_club.html')
+        form = response.context['form']
+        self.assertTrue(isinstance(form, ApplyToClubForm))
+        self.assertTrue(form.is_bound)
 
     def test_apply_with_invalid_form_input(self):
         self.data['personal_statement'] = ''
@@ -67,3 +92,19 @@ class ApplyToClubViewTestCase(TestCase):
         # Should send user back to application form.
         self.assertEqual(response.status_code, 200) # not a redirect. Same page.
         self.assertTemplateUsed(response, 'apply_to_club.html')
+
+    def test_successful_application(self):
+        self.client.login(username=self.user.username, password="Password123")
+        app_count_before = Application.objects.count()
+        response = self.client.post(self.url, self.data, follow=True)
+        app_count_after = Application.objects.count()
+        self.assertEqual(app_count_after, app_count_before+1)
+
+        # Should redirect user somewhere appropriate, indicating success.
+        response_url = reverse('show_clubs')
+        self.assertRedirects(
+            response, response_url,
+            status_code=302, target_status_code=200,
+            fetch_redirect_response=True
+        )
+        self.assertTemplateUsed(response, 'show_clubs.html')
