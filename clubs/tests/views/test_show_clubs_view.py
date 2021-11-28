b7f50576421ec31135cad8202f2e74d99d59da6e
Kieran Woolley <kieran.woolley1@gmail.com>
2021-11-28 21:30:32 +0000

    Add test for successful show clubs (list) request

--- clubs/tests/views/test_show_clubs_view.py
+++ clubs/tests/views/test_show_clubs_view.py
@@ -1,12 +1,17 @@
-"""Unit test for the feed view"""
+"""Unit test for the view allowing a list of clubs to be fetched and displayed."""
 
 from django.test import TestCase
 from django.urls import reverse
 
 class ShowClubsViewTestCase(TestCase):
-    """Test aspects of account view"""
+    """Test aspects of show clubs view"""
     def setUp(self):
         self.url = reverse('show_clubs')
 
     def test_get_show_clubs_url(self):
         self.assertEqual('/clubs/', self.url)
+
+    def test_show_clubs_request(self):
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, "show_clubs.html")
+        self.assertEqual(response.status_code, 200) #OK
