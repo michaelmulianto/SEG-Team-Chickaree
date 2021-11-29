b15714698143ea7c8a075f06fa1010c793cd9b97
Kieran Woolley <kieran.woolley1@gmail.com>
2021-11-29 17:02:33 +0000

    Setup change password view test case

--- clubs/tests/views/test_change_password_view.py
+++ clubs/tests/views/test_change_password_view.py
@@ -1,4 +1,8 @@
-"""Test backend implementation of password changing."""
+"""
+Test backend implementation of password changing.
+
+Note: a built in password change form is being used, hence the lack of tests specific to the front end.
+"""
 
 from django.test import TestCase
 from django.urls import reverse
@@ -8,6 +12,10 @@ from django.contrib.auth.hashers import check_password
 from clubs.tests.helpers import LogInTester
 
 class ChangePasswordViewTestCase(TestCase, LogInTester):
+    """Test all aspects of change password view."""
     fixtures = [
         'clubs/tests/fixtures/default_user.json',
     ]
+
+    def setUp(self):
+        self.user = User.objects.get(username="johndoe")
