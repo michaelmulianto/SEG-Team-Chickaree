d01283cfef8bfd0646c8d1a67c5510d6e4a9a2c7
John Kelly Tamargo <john.kelly.tamargo@gmail.com>
2021-11-29 23:24:37 +0000

    Add a max number of clubs shown per page

--- system/settings.py
+++ system/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'clubs',
     'widget_tweaks',
+    'bootstrap_pagination'
 ]
 
 MIDDLEWARE = [
@@ -142,3 +143,6 @@ MESSAGE_TAGS = {
 LOGIN_URL = 'log_in'
 
 REDIRECT_URL_WHEN_LOGGED_IN = 'show_clubs'
+
+#Page lengths
+CLUB123S_PER_PAGE = 10
