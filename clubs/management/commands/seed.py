24e526d5f3bc6f0edcf07062e029abf06556f88e
Kieran Woolley <kieran.woolley1@gmail.com>
2021-12-01 10:48:01 +0000

    Ensure seeder uses updated data, and have members created for each club

--- clubs/management/commands/seed.py
+++ clubs/management/commands/seed.py
@@ -1,8 +1,10 @@
 from django.core.management.base import BaseCommand, CommandError
 from faker import Faker
-from clubs.models import User, Club
+from random import sample, choice
+from clubs.models import User, Club, Member, Application
 
 class Command(BaseCommand):
+    """Fill the database with pseudorandom data"""
     def __init__(self):
         super().__init__()
         self.faker = Faker('en_GB')
@@ -20,13 +22,15 @@ class Command(BaseCommand):
                 first_name = first_name,
                 last_name = last_name,
                 email = email,
+                bio = bio,
+                experience = (i % 3) + 1,
                 password = "Password123",
             )
 
             user.full_clean()
             user.save()
 
-        for i in range(20):
+        for i in range(12):
             name = self.faker.unique.name()
             location = self.faker.country()
             description = self.faker.paragraph(nb_sentences=3)
@@ -36,6 +40,14 @@ class Command(BaseCommand):
                 location = location,
                 description = description
             )
-
             club.full_clean()
             club.save()
+
+            members = list(User.objects.all()).sample(items, (i%5)+3)
+            
+            for user in members:
+                Member.objects.create(
+                    club = club,
+                    user = user,
+                    is_officer = not(bool(i%4)),
+                )
