815ebde072787f65ddad41f82ba96167e603affa
Kieran Woolley <kieran.woolley1@gmail.com>
2021-12-04 15:28:18 +0000

    Amend redirect URL of edit account and add docstrings

--- clubs/views/account_views.py
+++ clubs/views/account_views.py
@@ -1,16 +1,28 @@
+"""Views relating to the currently logged in user's account."""
+
 from django.views import View
+from django.views.generic.edit import FormView, UpdateView
+
 from .helpers import login_prohibited
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
 from django.utils.decorators import method_decorator
-from django.views.generic.edit import FormView, UpdateView
+
 from clubs.forms import SignUpForm, EditAccountForm
 from clubs.models import User
+
+from django.contrib import messages
 from django.contrib.auth import login
 from django.urls import reverse
 from django.conf import settings
+from django.shortcuts import redirect
+
+@login_required
+def account(request):
+    """Render a page displaying the attributes of the currently logged in user."""
+    return render(request, 'account.html', {'user': request.user, 'my_clubs':get_clubs_of_user(request.user)})
 
 class SignUpView(FormView):
+    """Create a new user account."""
     form_class = SignUpForm
     template_name = "sign_up.html"
 
@@ -45,5 +57,5 @@ class EditAccountView(UpdateView):
     def get_success_url(self):
         """Return redirect URL after successful update."""
         messages.add_message(self.request, messages.SUCCESS, "Account Details updated!")
-        return reverse(settings.REDIRECT_URL_WHEN_LOGGED_IN)
+        return redirect('account')
 
--- clubs/views/static_views.py
+++ clubs/views/static_views.py
@@ -1,4 +1,6 @@
-from .helpers import login_prohibited
+"""Various small views."""
+
+from .helpers import login_prohibited, get_clubs_of_user
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
@@ -7,7 +9,3 @@ def home(request):
     """Generic splash page view for non-logged in user."""
     return render(request, 'home.html')
 
-@login_required
-def account(request):
-    """Render a page displaying the attributes of the currently logged in user."""
-    return render(request, 'account.html', {'user': request.user, 'my_clubs':get_clubs_of_user(request.user)})
