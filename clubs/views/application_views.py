2ac346cec75764032efc9a48867bb4d4b5481fd0
Kieran Woolley <kieran.woolley1@gmail.com>
2021-12-05 23:28:54 +0000

    Remove redundant code

--- clubs/views/application_views.py
+++ clubs/views/application_views.py
@@ -49,27 +49,6 @@ class ApplyToClubView(FormView):
         messages.add_message(self.request, messages.SUCCESS, f"Applied to {Club.objects.get(id=self.kwargs['club_id']).name}!")
         return reverse('show_clubs')
 
-# @login_required
-# @club_exists
-# @not_banned
-# def apply_to_club(request, club_id):
-#     """Have currently logged in user create an application to a specified club."""
-#     if request.method == 'POST':
-#         desired_club = Club.objects.get(id = club_id)
-#         current_user = request.user
-#         form = ApplyToClubForm(request.POST)
-#         if form.is_valid():
-#             if not(Application.objects.filter(club=desired_club, user = current_user).exists()) and not(Member.objects.filter(club=desired_club, user = current_user).exists()):
-#                 form.save(desired_club, current_user)
-#                 return redirect('show_clubs')
-#         # Else: Invalid form/Already applied/Already member
-#         if Application.objects.filter(club=desired_club, user = current_user).exists():
-#             messages.add_message(request, messages.ERROR, "You have already applied for this club")
-#         elif Member.objects.filter(club=desired_club, user = current_user).exists():
-#             messages.add_message(request, messages.ERROR, "You are already a member in this club")
-#         return render(request, 'apply_to_club.html', {'form': form, 'club':desired_club, 'my_clubs':get_clubs_of_user(request.user)})
-#     else: # GET
-#         return render(request, 'apply_to_club.html', {'form': ApplyToClubForm(), 'club':Club.objects.get(id = club_id), 'my_clubs':get_clubs_of_user(request.user)})
 
 @login_required
 @club_exists
