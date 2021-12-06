2594adb009bb7a996626cf52e7e4488795c7689a
John Kelly Tamargo <john.kelly.tamargo@gmail.com>
2021-12-06 16:57:20 +0000

    Change member to membership in views.py

--- clubs/views.py
+++ clubs/views.py
@@ -12,7 +12,7 @@ from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
 from django.conf import settings
 from clubs.forms import LogInForm, SignUpForm, CreateClubForm, EditAccountForm, ApplyToClubForm, EditClubInfoForm
-from clubs.models import User, Club, Application, Member, Ban
+from clubs.models import User, Club, Application, Membership, Ban
 from clubs.helpers import login_prohibited, club_exists, application_exists, membership_exists, ban_exists, not_banned, is_user_officer_of_club, is_user_owner_of_club, get_clubs_of_user
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
@@ -76,7 +76,7 @@ def my_clubs_list(request):
     for club in Club.objects.all():
         if Application.objects.filter(club=club, user=current_user):
             my_clubs.append(club)
-        if Member.objects.filter(club=club, user=current_user):
+        if Membership.objects.filter(club=club, user=current_user):
             my_clubs.append(club)
 
     paginator = Paginator(my_clubs, settings.CLUBS_PER_PAGE)
@@ -115,7 +115,7 @@ def create_club(request):
         form = CreateClubForm(request.POST)
         if form.is_valid():
             new_club = form.save()
-            Member.objects.create(
+            Membership.objects.create(
                 club = new_club,
                 user = current_user,
                 is_owner = True
@@ -134,13 +134,13 @@ def apply_to_club(request, club_id):
         current_user = request.user
         form = forms.ApplyToClubForm(request.POST)
         if form.is_valid():
-            if not(Application.objects.filter(club=desired_club, user = current_user).exists()) and not(Member.objects.filter(club=desired_club, user = current_user).exists()):
+            if not(Application.objects.filter(club=desired_club, user = current_user).exists()) and not(Membership.objects.filter(club=desired_club, user = current_user).exists()):
                 form.save(desired_club, current_user)
                 return redirect('show_clubs')
         # Else: Invalid form/Already applied/Already member
         if Application.objects.filter(club=desired_club, user = current_user).exists():
             messages.add_message(request, messages.ERROR, "You have already applied for this club")
-        elif Member.objects.filter(club=desired_club, user = current_user).exists():
+        elif Membership.objects.filter(club=desired_club, user = current_user).exists():
             messages.add_message(request, messages.ERROR, "You are already a member in this club")
         return render(request, 'apply_to_club.html', {'form': form, 'club':desired_club, 'my_clubs':get_clubs_of_user(request.user)})
     else: # GET
@@ -163,29 +163,29 @@ def leave_club(request, club_id):
     """Delete the member object linking the current user to the specified club, iff it exists."""
     current_user = request.user
     applied_club = Club.objects.get(id=club_id)
-    if Member.objects.filter(club=applied_club, user = current_user).exists():
-        Member.objects.get(club=applied_club, user=current_user).delete()
+    if Membership.objects.filter(club=applied_club, user = current_user).exists():
+        Membership.objects.get(club=applied_club, user=current_user).delete()
     return redirect('show_clubs')
 
 @login_required
 @membership_exists
 def kick_member(request, member_id):
     current_user = request.user
-    member = Member.objects.get(id=member_id)
+    member = Membership.objects.get(id=member_id)
     club = member.club
     if is_user_owner_of_club(current_user, club) or is_user_officer_of_club(current_user, club):
-        Member.objects.filter(id=member_id).delete()
+        Membership.objects.filter(id=member_id).delete()
     return redirect('members_list', club_id=club.id)
 
 @login_required
 @membership_exists
 def ban_member(request, member_id):
     current_user = request.user
-    member = Member.objects.get(id=member_id)
+    member = Membership.objects.get(id=member_id)
     club = member.club
     if is_user_owner_of_club(current_user, club) and not is_user_officer_of_club(member.user, club): #Only owners can ban members, not officers.
         Ban.objects.create(club=club, user=member.user)
-        Member.objects.filter(id=member_id).delete()
+        Membership.objects.filter(id=member_id).delete()
     return redirect('members_list', club_id=club.id)
 
 @login_required
@@ -195,10 +195,10 @@ def unban_member(request, ban_id):
     ban = Ban.objects.get(id=ban_id)
     club = ban.club
     if is_user_owner_of_club(current_user, club):
-        Member.objects.create(club=club, user=ban.user)
+        Membership.objects.create(club=club, user=ban.user)
         Ban.objects.filter(id=ban_id).delete()
     else:
-        member = Member.objects.get(club=club, is_owner=True)
+        member = Membership.objects.get(club=club, is_owner=True)
         messages.error(request, 'Only the owner can unban users. Please ask ' + member.user.first_name + ' ' + member.user.last_name + ' to perform this action for you.')
     return redirect('members_list', club_id=club.id)
 
@@ -219,7 +219,7 @@ def log_out(request):
 def show_applications_to_club(request, club_id):
     """Allow the owner of a club to view all applications to said club."""
     club_to_view = Club.objects.get(id = club_id)
-    if not(Member.objects.filter(club=club_to_view, user=request.user, is_owner=True).exists()):
+    if not(Membership.objects.filter(club=club_to_view, user=request.user, is_owner=True).exists()):
         # Access denied
         #return redirect('show_clubs', {'my_clubs':get_clubs_of_user(request.user)})
         return redirect('show_clubs')
@@ -246,13 +246,13 @@ def respond_to_application(request, app_id, is_accepted):
     """Allow the owner of a club to accept or reject some application to said club."""
     application = Application.objects.get(id = app_id)
     club_applied = application.club
-    if not(Member.objects.filter(club=application.club, user=request.user, is_owner=True).exists()):
+    if not(Membership.objects.filter(club=application.club, user=request.user, is_owner=True).exists()):
         # Access denied
         return redirect('show_clubs')
     # Create member object iff application is accepted
 
     if is_accepted:
-        Member.objects.create(
+        Membership.objects.create(
             user = application.user,
             club = club_applied
         )
@@ -283,7 +283,7 @@ def show_club(request, club_id):
     """View details of a club."""
     current_user = request.user
     club = Club.objects.get(id=club_id)
-    members = Member.objects.filter(club = club_id)
+    members = Membership.objects.filter(club = club_id)
     officers = members.filter(is_officer = True)
     officerCount = officers.count()
     getOwner = members.get(is_owner = True)
@@ -304,9 +304,9 @@ def show_club(request, club_id):
 @membership_exists
 def promote_member_to_officer(request, member_id):
     """Allow the owner of a club to promote some member of said club to officer."""
-    member = Member.objects.get(id = member_id)
+    member = Membership.objects.get(id = member_id)
     club = member.club
-    if not(Member.objects.filter(club=club, user=request.user, is_owner=True).exists()):
+    if not(Membership.objects.filter(club=club, user=request.user, is_owner=True).exists()):
         # Access denied, member isn't owner
         return redirect('show_clubs')
     member.is_officer = True
@@ -317,9 +317,9 @@ def promote_member_to_officer(request, member_id):
 @membership_exists
 def demote_officer_to_member(request, member_id):
     """Allow the owner of a club to promote some member of said club to officer."""
-    member = Member.objects.get(id = member_id)
+    member = Membership.objects.get(id = member_id)
     club = member.club
-    if not(Member.objects.filter(club=club, user=request.user, is_owner=True).exists()):
+    if not(Membership.objects.filter(club=club, user=request.user, is_owner=True).exists()):
         # Access denied, member isn't owner
         return redirect('show_clubs')
     member.is_officer = False
@@ -332,7 +332,7 @@ def members_list(request, club_id):
     """Display a list of the members in a club"""
     current_user = request.user
     club = Club.objects.get(id = club_id)
-    members = Member.objects.filter(club = club)
+    members = Membership.objects.filter(club = club)
     return render(request, 'members_list.html', {'members': members, 'club': club, 'current_user': current_user , 'my_clubs':get_clubs_of_user(request.user)})
 
 
@@ -340,13 +340,13 @@ def members_list(request, club_id):
 @membership_exists
 def transfer_ownership_to_officer(request, member_id):
     """Allow the owner of a club to promote some member of said club to officer."""
-    member = Member.objects.get(id = member_id)
+    member = Membership.objects.get(id = member_id)
     club = member.club
-    if not(Member.objects.filter(club=club, user=request.user, is_owner=True).exists()):
+    if not(Membership.objects.filter(club=club, user=request.user, is_owner=True).exists()):
         # Access denied, member isn't owner
         return redirect('members_list', club_id=club.id)
 
-    curr_owner = Member.objects.get(club=club, user=request.user)
+    curr_owner = Membership.objects.get(club=club, user=request.user)
     if not(member.is_officer):
         # Targetted member should be an officer
         return redirect('members_list', club_id=club.id)
