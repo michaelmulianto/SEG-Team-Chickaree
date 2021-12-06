7c917605beabb3566732f444fc497a739cc96220
Kieran Woolley <kieran.woolley1@gmail.com>
2021-12-06 00:07:42 +0000

    Restore deletion of manage_club

--- clubs/views/club_detail_views.py
+++ clubs/views/club_detail_views.py
@@ -29,6 +29,7 @@ def show_club(request, club_id):
     club = Club.objects.get(id=club_id)
     members = Member.objects.filter(club = club_id)
     officers = members.filter(is_officer = True)
+    officer_count = officers.count()
     get_owner = members.get(is_owner = True)
     num_members = members.count()
     check_user_is_member = members.filter(user = current_user)
@@ -41,15 +42,7 @@ def show_club(request, club_id):
         is_member = True
     if check_user_is_member.filter(is_officer = True).count() > 0:
         is_officer = True
-    return render(request, 'show_club.html', {'club': club, 'members': num_members, 'userIsMember': is_member, 'owner': get_owner, 'userIsOwner': is_owner, 'userIsOfficer': is_officer, 'officers': officers, 'my_clubs':get_clubs_of_user(request.user)})
-
-
-@login_required
-@club_exists
-def manage_club(request, club_id):
-    """Access management page of club"""
-    club = Club.objects.get(id=club_id)
-    return render(request, 'manage_club.html', {'club': club, 'my_clubs':get_clubs_of_user(request.user)})
+    return render(request, 'show_club.html', {'club': club, 'members': num_members, 'userIsMember': is_member, 'owner': get_owner, 'userIsOwner': is_owner, 'userIsOfficer': is_officer, 'officers': officers, 'officerCount': officer_count, 'my_clubs':get_clubs_of_user(request.user)})
 
 
 @login_required
