e6601725d699526cb4ee64e1f2b0473fdccc92f2
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2021-12-10 14:32:11 +0000

    Template tags for tournament lists

--- clubs/templatetags/define_action.py
+++ clubs/templatetags/define_action.py
@@ -1,51 +1,68 @@
 from django import template
-from clubs.models import User, Club, Application, Membership, Ban
+from datetime import datetime
+from clubs.models import User, Club, Application, Membership, Ban, Participant, Tournament
 register = template.Library()
 
 @register.simple_tag
 def count_members(club_to_count):
-  return Membership.objects.filter(club=club_to_count).count()
+    return Membership.objects.filter(club=club_to_count).count()
+
+@register.simple_tag
+def count_participants(tournament):
+    return Participant.objects.filter(tournament=tournament).count()
+
+
 
 @register.simple_tag
 def get_clubs(current_user):
-  memberships = Membership.objects.filter(user=current_user)
-  my_clubs = []
-  for membership in memberships:
+    memberships = Membership.objects.filter(user=current_user)
+    my_clubs = []
+    for membership in memberships:
       my_clubs.append(membership.club)
-  return my_clubs
+    return my_clubs
+
+@register.simple_tag
+def get_tournaments(club, date_range_type="ongoing"):
+    if(date_range_type=="ongoing"):
+        today = datetime.now()
+        return Tournament.objects.filter(club=club, start__lte=today, end__gte=today)
+    return Tournament.objects.all()
+
 
 @register.simple_tag
 def get_members(club):
-  return Membership.objects.filter(club=club)
+    return Membership.objects.filter(club=club)
 
 @register.simple_tag
 def get_banned_members(club):
-  return Ban.objects.filter(club=club)
+    return Ban.objects.filter(club=club)
 
 @register.simple_tag
 def get_applications(club):
-  return Application.objects.filter(club=club)
+    return Application.objects.filter(club=club)
 
 @register.simple_tag
 def get_officers(club):
-  return Membership.objects.filter(club=club, is_officer=True)
+    return Membership.objects.filter(club=club, is_officer=True)
 
 @register.simple_tag
 def get_owner(club):
-  return Membership.objects.get(club=club, is_owner=True)
+    return Membership.objects.get(club=club, is_owner=True)
+
+
 
 @register.simple_tag
 def check_has_applied(club_to_check, user):
-  return Application.objects.filter(club=club_to_check, user=user).exists()
+    return Application.objects.filter(club=club_to_check, user=user).exists()
 
 @register.simple_tag
 def check_is_member(club_to_check, user):
-  return Membership.objects.filter(club=club_to_check, user=user).exists()
+    return Membership.objects.filter(club=club_to_check, user=user).exists()
 
 @register.simple_tag
 def check_is_officer(club_to_check, user):
-  return Membership.objects.filter(club=club_to_check, user=user, is_officer=True).exists()
+    return Membership.objects.filter(club=club_to_check, user=user, is_officer=True).exists()
 
 @register.simple_tag
 def check_is_owner(club_to_check, user):
-  return Membership.objects.filter(club=club_to_check, user=user, is_owner=True).exists()
+    return Membership.objects.filter(club=club_to_check, user=user, is_owner=True).exists()
