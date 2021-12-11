0ea02053884c6c421a344683f7adef2a7abe59d0
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2021-12-11 15:27:46 +0000

    Add url to show tournament

--- system/urls.py
+++ system/urls.py
@@ -59,4 +59,6 @@ urlpatterns = [
     path('member/<int:member_id>/transfer_ownership/', views.transfer_ownership_to_officer, name='transfer_ownership_to_officer'),
     path('member/<int:member_id>/kick/', views.kick_member, name='kick_member'),
     path('member/<int:member_id>/ban/', views.ban_member, name='ban_member'),
+
+    path('tournament/<int:club_id>/', views.show_tournament, name='show_tournament'),
 ]
