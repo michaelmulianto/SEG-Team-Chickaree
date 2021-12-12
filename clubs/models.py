a8e6981d7327514d4435efecc81c8936b7effdac
Michael Mulianto <michaelmulianto@gmail.com>
2021-12-12 14:04:19 +0800

    add validators

--- clubs/models.py
+++ clubs/models.py
@@ -176,7 +176,7 @@ class Tournament(models.Model):
         (48, '48'),
         (96, '96'),
     )
-    capacity = models.IntegerField(default = 2, choices = NUMBERS)
+    capacity = models.PositiveIntegerField(default = 2, choices = NUMBERSblank=False, validators=[MinValueValidator(2), MaxValueValidator(96)])
 
     def __str__(self):
         return f'{self.name} by {self.club}'
