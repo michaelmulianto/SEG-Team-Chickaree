ee3b6b6f9030094812cca099c18b37a0f0617a61
John Kelly Tamargo <john.kelly.tamargo@gmail.com>
2021-12-12 22:31:34 +0000

    Make a helper to check a user is lead organiser of a tournament

--- clubs/views/helpers.py
+++ clubs/views/helpers.py
@@ -1,5 +1,7 @@
 """Functions to aid functionality of the views"""
-from clubs.models import Club, Membership
+from django.core.exceptions import ObjectDoesNotExist
+from clubs.models import Club, Membership, Organiser
+from clubs.views.decorators import is_head_organiser
 
 
 def is_user_officer_of_club(user, club):
@@ -7,6 +9,15 @@ def is_user_officer_of_club(user, club):
 
 def is_user_owner_of_club(user, club):
     return Membership.objects.filter(user=user, club=club, is_owner=True).exists()
+
+def is_head_organiser_of_tournament(user, tournament):
+    try:
+        possible_organiser_member = Membership.objects.get(club = tournament.club, user = user)
+    except(ObjectDoesNotExist):
+        return False
+    else:
+        return Organiser.objects.filter(member = possible_organiser_member, tournament = tournament, is_head_organiser = True).exists()
+
 
 def sort_clubs(param, order):
     if order == None:
