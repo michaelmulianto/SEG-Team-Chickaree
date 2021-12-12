64f2eaf82de44b8c2226783f647520c2c4b67117
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2021-12-12 13:53:46 +0000

    Add groupstage to admin models

--- clubs/admin.py
+++ clubs/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Club, Application, Membership, Ban, Tournament, Participant, Organiser
+from .models import User, Club, Application, Membership, Ban, Tournament, Participant, Organiser, GroupStage
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
@@ -57,4 +57,10 @@ class ParticipantAdmin(admin.ModelAdmin):
 class OrganiserAdmin(admin.ModelAdmin):
     list_display = [
              'member', 'tournament', 'is_lead_organiser'
+    ]
+
+@admin.register(GroupStage)
+class GroupStageAdmin(admin.ModelAdmin):
+    list_display = [
+             'tournament'
     ]
