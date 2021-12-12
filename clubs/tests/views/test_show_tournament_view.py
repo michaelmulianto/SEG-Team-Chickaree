26f72ee026188a4d1e338b4214ae1dcaa175d34c
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2021-12-12 00:21:15 +0000

    Add test cases for show tournament

--- clubs/tests/views/test_show_tournament_view.py
+++ clubs/tests/views/test_show_tournament_view.py
@@ -2,44 +2,65 @@
 
 from django.test import TestCase
 from django.urls import reverse
-from clubs.models import Club, Membership, User
+from clubs.models import Club, Membership, User, Tournament, Organiser
 from clubs.tests.helpers import reverse_with_next, MenuTesterMixin
 
 class ShowClubViewTestCase(TestCase, MenuTesterMixin):
     """Test aspects of show tournament view"""
 
-    fixtures = ['clubs/tests/fixtures/default_club.json',
-    'clubs/tests/fixtures/default_user.json']
+    fixtures = [
+        'clubs/tests/fixtures/default_club.json',
+        'clubs/tests/fixtures/default_user.json',
+        'clubs/tests/fixtures/other_users.json',
+        'clubs/tests/fixtures/default_tournament.json',
+    ]
 
     def setUp(self):
+        self.owner_user = User.objects.get(username='johndoe')
+        self.non_member_user = User.objects.get(username='janedoe')
         self.club = Club.objects.get(name='King\'s Knights')
-        self.url = reverse('show_tournament', kwargs={'tournament_id': self.tournament.id})
-        self.member = Membership.objects.create(
-            user = User.objects.get(username='johndoe'),
+
+        self.owner_member = Membership.objects.create(
             club = self.club,
+            user = self.owner_user,
             is_owner = True,
-            is_officer = False
         )
+
+        self.tournament = Tournament.objects.get(name="Grand Championship")
+
+        self.organiser = Organiser.objects.create(
+            member = self.owner_member,
+            tournament = self.tournament
+        )
+
+
+        self.url = reverse('show_tournament', kwargs={'tournament_id': self.club.id})
 
     def test_show_tournament_url(self):
         self.assertEqual(self.url, f'/tournament/{self.tournament.id}/')
 
-
-    def test_get_show_tournament_with_valid_id(self):
-        self.client.login(email="johndoe@example.org", password="Password123")
-        response = self.client.get(self.url)
-        self.assertTemplateUsed(response, "show_tournament.html")
-        self.assertEqual(response.status_code, 200) #OK
-        self.assert_menu(response)
-
-    def test_promote_redirects_when_not_logged_in(self):
+    def test_show_tournament_redirects_when_not_logged_in(self):
         response = self.client.get(self.url)
         redirect_url = reverse_with_next('log_in', self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
     def test_get_show_tournament_with_invalid_id(self):
-        self.client.login(email="johndoe@example.org", password="Password123")
+        self.client.login(email=self.owner_user.email, password="Password123")
         self.url = reverse('show_tournament', kwargs={'tournament_id': self.tournament.id-1})
         response = self.client.get(self.url)
-        redirect_url = reverse("show_tournaments")
+        redirect_url = reverse("show_clubs")
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
+
+    def test_show_tournament_redirects_when_not_member(self):
+        self.client.login(email=self.non_member_user.email, password="Password123")
+        self.url = reverse('show_tournament', kwargs={'tournament_id': self.tournament.id})
+        response = self.client.get(self.url)
+        redirect_url = reverse("show_club", kwargs={'club_id': self.club.id})
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
+
+    def test_get_show_tournament_with_valid_id(self):
+        self.client.login(email=self.owner_user.email, password="Password123")
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, "show_tournament.html")
+        self.assertEqual(response.status_code, 200) #OK
+        self.assert_menu(response)
