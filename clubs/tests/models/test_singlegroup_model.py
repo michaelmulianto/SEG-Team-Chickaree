ccd2fcd9fa84a4dbf6e63bd00c93bb18589e7e35
Kieran Woolley <kieran.woolley1@gmail.com>
2021-12-14 14:14:34 +0000

    Amend existing tests to utlilise a full set of participants

--- clubs/tests/models/test_singlegroup_model.py
+++ clubs/tests/models/test_singlegroup_model.py
@@ -9,20 +9,16 @@ class SingleGroupModelTestCase(TestCase):
 
     fixtures = ['clubs/tests/fixtures/default_user.json',
     'clubs/tests/fixtures/default_club.json',
-    'clubs/tests/fixtures/default_tournament.json'
+    'clubs/tests/fixtures/default_tournament.json',
+    'clubs/tests/fixtures/default_tournament_participants.json'
     ]
 
     # Test setup
     def setUp(self):
         self.user = User.objects.get(username='johndoe')
-        self.stage = GroupStage.objects.create(
-            tournament = Tournament.objects.get(name = "Grand Championship"),
-            round_num = 1
-        )
-        self.group = SingleGroup.objects.create(
-            group_stage = self.stage,
-            winners_required = 1
-        )
+        self.tournament = Tournament.objects.get(name = "Grand Championship")
+        self.stage = self.tournament.generate_next_round()
+        self.group = SingleGroup.objects.filter(group_stage=self.stage)[0]
     
     def test_valid_singlegroup(self):
         self._assert_valid_singlegroup()
@@ -38,17 +34,27 @@ class SingleGroupModelTestCase(TestCase):
 
     def test_single_group_deletes_when_group_stage_is_deleted(self):
         self.stage.delete()
-        self.assertFalse(SingleGroup.objects.filter(group_stage=self.group.group_stage).exists())
+        self.assertFalse(SingleGroup.objects.filter(id=self.group.id).exists())
 
     def test_group_stage_does_not_delete_when_single_group_is_deleted(self):
         self.group.delete()
-        self.assertTrue(GroupStage.objects.filter(tournament = self.stage.tournament).exists())
+        self.assertTrue(GroupStage.objects.filter(id=self.stage.id).exists())
 
     # Test winners_required
     def test_winners_required_must_not_be_blank(self):
         self.group.winners_required = None
-        with self.assertRaises(ValidationError):
-            self.group.full_clean()
+        self._assert_invalid_singlegroup()
+
+    # Mischallaneous Validation
+    def test_one_player_plays_too_many_games_and_one_not_enough(self):
+        pass
+    
+    def test_too_few_matches_assigned_to_group(self):
+        self.group.get_matches()[0].delete()
+        self._assert_invalid_singlegroup()
+
+    def test_too_many_games_assigned_to_group(self):
+        pass
 
     # Assertions
     def _assert_valid_singlegroup(self):
