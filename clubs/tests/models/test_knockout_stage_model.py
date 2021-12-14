a652f9206931b600e2c24b13eb9a35176ee10812
John Kelly Tamargo <john.kelly.tamargo@gmail.com>
2021-12-14 17:42:14 +0000

    Test knockout stage model

--- clubs/tests/models/test_knockout_stage_model.py
+++ clubs/tests/models/test_knockout_stage_model.py
@@ -1,20 +1,15 @@
 """Tests for single group model, found in clubs/models.py"""
 
 from django.test import TestCase
-from clubs.models import Match, Participant, Tournament, Club, User, Membership, GroupStage, SingleGroup,  KnockoutStage
+from clubs.models import Participant, Tournament, KnockoutStage
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
-from django.db.utils import IntegrityError
 
 
 class KnockOutStageModelTestCase(TestCase):
     """Test all aspects of a single group model."""
 
-    fixtures = ['clubs/tests/fixtures/default_user.json',
-                'clubs/tests/fixtures/other_users.json',
-                'clubs/tests/fixtures/default_club.json',
-                'clubs/tests/fixtures/other_clubs.json',
+    fixtures = ['clubs/tests/fixtures/default_club.json',
                 'clubs/tests/fixtures/default_tournament.json',
-                'clubs/tests/fixtures/other_tournaments.json',
                 'clubs/tests/fixtures/default_tournament_participants.json'
                 ]
 
@@ -85,33 +80,143 @@ class KnockOutStageModelTestCase(TestCase):
     
     # Tests on StageMethodInterface parent class
     def test_get_winners_does_return_NotImplementedError(self):
-        pass
+        try:
+            self.knockoutStage.get_winners()
+        except(NotImplementedError):
+            self.fail("get_winners() has not been implemented on knockoutStage")
+
 
     def test_get_matches_returns_the_all_the_matches_in_the_knockout_stage(self):
-        pass
+        self.knockoutStage.delete() #Create a knockout stage from within a tournament
+        self.tournament.capacity = 16
+        self._adjust_num_participants_to_capacity()
+        knockout_round = self.tournament.generate_next_round()
+        self.assertIsInstance(knockout_round, KnockoutStage)
+        self.assertEqual(len(knockout_round.get_matches()), 8)
+        self.assertEqual(len(set(knockout_round.get_player_occurences())), 16)
 
     def test_get_matches_does_not_return_matches_in_other_stages(self):
-        pass
+        self.knockoutStage.delete() #Create a knockout stage from within a tournament
+        self.tournament.capacity = 16
+        self._adjust_num_participants_to_capacity()
+        first_knockout_round = self.tournament.generate_next_round()
+        self.assertIsInstance(first_knockout_round, KnockoutStage)
+        first_set_of_matches = first_knockout_round.get_matches()
+        self.assertEqual(len(first_set_of_matches), 8)
+        self.assertEqual(len(set(first_knockout_round.get_player_occurences())), 16)
 
-    def test_get_player_occurences_returns_all_the_player_occurences_in_the_knockout_stage(self):
-        pass
+        self._complete_round_with_single_matchset(first_knockout_round)
+
+        second_knockout_round = self.tournament.generate_next_round()
+        self.assertIsInstance(second_knockout_round, KnockoutStage)
+        second_set_of_matches = second_knockout_round.get_matches()
+        self.assertEqual(len(second_set_of_matches), 4)
+        self.assertEqual(len(set(second_knockout_round.get_player_occurences())), 8)
+
+        self.assertTrue(set(first_set_of_matches).isdisjoint(second_set_of_matches))
 
     def test_there_are_no_repeated_occurences_of_players_in_the_knockout_stage(self):
-        pass
+        self.knockoutStage.delete() #Create a knockout stage from within a tournament
+        self.tournament.capacity = 16
+        self._adjust_num_participants_to_capacity()
+        knockout_round = self.tournament.generate_next_round()
+        self.assertIsInstance(knockout_round, KnockoutStage)
+        first_set_of_matches = knockout_round.get_matches()
+        self.assertEqual(len(first_set_of_matches), 8)
+        self.assertEqual(len(set(knockout_round.get_player_occurences())), 16)
+        self.assertEqual(len(knockout_round.get_player_occurences()), len(set(knockout_round.get_player_occurences())))
 
     def test_incomplete_round_returns_false(self):
-        pass
+        self.knockoutStage.delete() #Create a knockout stage from within a tournament
+        self.tournament.capacity = 16
+        self._adjust_num_participants_to_capacity()
+        knockout_round = self.tournament.generate_next_round()
+        self.assertIsInstance(knockout_round, KnockoutStage)
+        first_set_of_matches = knockout_round.get_matches()
+        self.assertEqual(len(first_set_of_matches), 8)
+        self.assertEqual(len(set(knockout_round.get_player_occurences())), 16)
+
+        matches = knockout_round.get_matches()
+        i = 0
+        for match in matches:
+            if(i != len(matches) -1): # complete all matches except one
+                break
+            match.result = 1
+            match.save()
+            i += 1
+
+        self.assertFalse(knockout_round.get_is_complete())
 
     def test_complete_round_returns_true(self):
-        pass
+        self.knockoutStage.delete() #Create a knockout stage from within a tournament
+        self.tournament.capacity = 16
+        self._adjust_num_participants_to_capacity()
+        knockout_round = self.tournament.generate_next_round()
+        self.assertIsInstance(knockout_round, KnockoutStage)
+        first_set_of_matches = knockout_round.get_matches()
+        self.assertEqual(len(first_set_of_matches), 8)
+        self.assertEqual(len(set(knockout_round.get_player_occurences())), 16)
+
+        self._complete_round_with_single_matchset(knockout_round)
+
+        self.assertTrue(knockout_round.get_is_complete())
 
     # Tests on knockout stage
     def test_get_winners_returns_all_the_winners_of_the_round(self):
-        pass
+        self.knockoutStage.delete() #Create a knockout stage from within a tournament
+        self.tournament.capacity = 16
+        self._adjust_num_participants_to_capacity()
+        knockout_round = self.tournament.generate_next_round()
+        self.assertIsInstance(knockout_round, KnockoutStage)
+        first_set_of_matches = knockout_round.get_matches()
+        self.assertEqual(len(first_set_of_matches), 8)
+        self.assertEqual(len(set(knockout_round.get_player_occurences())), 16)
+
+        matches = knockout_round.get_matches()
+        artificial_winners = []
+        for match in matches:
+            match.result = 1
+            match.save()
+            artificial_winners.append(match.white_player)
+
+        self.assertFalse(set(artificial_winners).isdisjoint(set(knockout_round.get_winners())))
 
     def test_incomplete_round_returns_none_on_get_winners(self):
-        pass
+        self.knockoutStage.delete() #Create a knockout stage from within a tournament
+        self.tournament.capacity = 16
+        self._adjust_num_participants_to_capacity()
+        knockout_round = self.tournament.generate_next_round()
+        self.assertIsInstance(knockout_round, KnockoutStage)
+        first_set_of_matches = knockout_round.get_matches()
+        self.assertEqual(len(first_set_of_matches), 8)
+        self.assertEqual(len(set(knockout_round.get_player_occurences())), 16)
 
+        matches = knockout_round.get_matches()
+        i = 0
+        for match in matches:
+            if(i != len(matches) -1): # complete all matches except one
+                break
+            match.result = 1
+            match.save()
+            i += 1
+
+        self.assertEqual(knockout_round.get_winners(), None)
+
+    # Helpers
+    def _adjust_num_participants_to_capacity(self):
+        participants = Participant.objects.filter(tournament=self.tournament)
+        i = 0
+        for p in participants:
+            if i >= participants.count()-self.tournament.capacity:
+                break
+            p.delete()
+            i += 1
+
+    def _complete_round_with_single_matchset(self, my_round):
+        matches = my_round.get_matches()
+        for match in matches:
+            match.result = 1 # Let white win, we don't care about specifics
+            match.save()
 
     # Assertions
     def _assert_valid_knockout_stage(self):
