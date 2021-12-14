058362c9a397dee7a38746e72d59cecde7e989aa
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2021-12-14 20:22:25 +0000

    Reorganise and remove redundant template methods

--- clubs/templatetags/define_action.py
+++ clubs/templatetags/define_action.py
@@ -18,6 +18,27 @@ def boolean_or(a, b):
     return a or b
 
 
+# CLUB tags
+
+@register.simple_tag
+def check_has_applied(club_to_check, user):
+    return Application.objects.filter(club=club_to_check, user=user).exists()
+
+@register.simple_tag
+def check_is_member(club_to_check, user):
+    return Membership.objects.filter(club=club_to_check, user=user).exists()
+
+@register.simple_tag
+def check_is_officer(club_to_check, user):
+    return Membership.objects.filter(club=club_to_check, user=user, is_officer=True).exists()
+
+@register.simple_tag
+def check_is_owner(club_to_check, user):
+    return Membership.objects.filter(club=club_to_check, user=user, is_owner=True).exists()
+
+
+# TOURNAMENT tags
+
 @register.simple_tag
 def check_is_organiser(user, tournament):
     if Membership.objects.filter(user=user, club=tournament.club).exists():
@@ -41,37 +62,3 @@ def check_has_joined_tournament(user, tournament):
         return Participant.objects.filter(member=membership, tournament=tournament).exists()
     else:
         return False
-# CLUB tags
-
-@register.simple_tag
-def check_has_applied(club_to_check, user):
-    return Application.objects.filter(club=club_to_check, user=user).exists()
-
-@register.simple_tag
-def check_is_member(club_to_check, user):
-    return Membership.objects.filter(club=club_to_check, user=user).exists()
-
-@register.simple_tag
-def check_is_officer(club_to_check, user):
-    return Membership.objects.filter(club=club_to_check, user=user, is_officer=True).exists()
-
-@register.simple_tag
-def check_is_owner(club_to_check, user):
-    return Membership.objects.filter(club=club_to_check, user=user, is_owner=True).exists()
-
-
-# TOURNAMENT tags
-
-@register.simple_tag
-def check_is_lead_organiser(user, tournament):
-    if Membership.objects.filter(user=user, club=tournament.club).exists():
-        membership = Membership.objects.get(user=user, club=tournament.club)
-        return Organiser.objects.filter(member=membership, tournament=tournament).exists()
-    return False
-
-@register.simple_tag
-def check_has_joined_tournament(user, tournament):
-    if Membership.objects.filter(user=user, club=tournament.club).exists():
-        membership = Membership.objects.get(user=user, club=tournament.club)
-        return Participant.objects.filter(member=membership, tournament=tournament).exists()
-    return False
