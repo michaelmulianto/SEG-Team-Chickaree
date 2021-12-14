dc5c11bf359c90bb8671ca8e207c4414231123a0
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2021-12-14 14:41:08 +0000

    Add test cases for show tournament participants view

--- clubs/tests/views/tournament_views/test_show_tournament_participants_view.py
+++ clubs/tests/views/tournament_views/test_show_tournament_participants_view.py
@@ -47,3 +47,35 @@ class ShowClubViewTestCase(TestCase, MenuTesterMixin):
         )
 
         self.url = reverse('show_tournament_participants', kwargs={'tournament_id': self.tournament.id})
+
+    def test_show_tournament_participants_url(self):
+        self.assertEqual(self.url, f'/tournament/{self.tournament.id}/participants')
+
+    def test_show_tournament_participants_redirects_when_not_logged_in(self):
+        response = self.client.get(self.url)
+        redirect_url = reverse_with_next('log_in', self.url)
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
+
+    def test_get_show_tournament_participants_with_invalid_id(self):
+        self.client.login(email=self.owner_user.email, password="Password123")
+        self.url = reverse('show_tournament_participants', kwargs={'tournament_id': self.tournament.id-1})
+        response = self.client.get(self.url)
+        redirect_url = reverse("show_clubs")
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
+        self.assertTemplateUsed(response, "show_clubs.html")
+        self.assert_menu(response)
+
+    def test_show_tournament_participants_redirects_when_not_member(self):
+        self.client.login(email=self.non_member_user.email, password="Password123")
+        response = self.client.get(self.url)
+        redirect_url = reverse("show_club", kwargs={'club_id': self.club.id})
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
+        self.assertTemplateUsed(response, "show_club.html")
+        self.assert_menu(response)
+
+    def test_get_show_tournament_participants_with_valid_id(self):
+        self.client.login(email=self.owner_user.email, password="Password123")
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200) #OK
+        self.assertTemplateUsed(response, "show_tournament_participants.html")
+        self.assert_menu(response)
