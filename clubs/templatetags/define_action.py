28384e9eb09adfb4b63ae6e6ec890f8d26175eba
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2021-12-14 20:21:57 +0000

    Add documentation for template tag methods

--- clubs/templatetags/define_action.py
+++ clubs/templatetags/define_action.py
@@ -1,6 +1,17 @@
+"""
+Django provides many built in tags to use within templates but some may be
+missing and they are implemented here.
+
+In addition, functions in templates cannot take parameters so when a method
+would need to more than one parameter it may be implemented here to avoid
+passing in many variables in the view.
+"""
+
 from django import template
 from clubs.models import Club, Application, Membership, Participant, Organiser
 register = template.Library()
+
+# BASIC tags
 
 @register.simple_tag
 def boolean_or(a, b):
@@ -30,6 +41,7 @@ def check_has_joined_tournament(user, tournament):
         return Participant.objects.filter(member=membership, tournament=tournament).exists()
     else:
         return False
+# CLUB tags
 
 @register.simple_tag
 def check_has_applied(club_to_check, user):
@@ -48,6 +60,7 @@ def check_is_owner(club_to_check, user):
     return Membership.objects.filter(club=club_to_check, user=user, is_owner=True).exists()
 
 
+# TOURNAMENT tags
 
 @register.simple_tag
 def check_is_lead_organiser(user, tournament):
