1ea6e604d605c8f5d2ba6c9cbd47ebd2207c69d5
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2021-12-15 01:51:54 +0000

    Relocate, rename and document add organiser

--- clubs/views/tournament_management_views.py
+++ clubs/views/tournament_management_views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
 from .decorators import match_exists, tournament_exists
-from .helpers import is_lead_organiser_of_tournament
+from .helpers import is_lead_organiser_of_tournament, is_user_organiser_of_tournament, is_participant_in_tournament, is_user_owner_of_club, is_user_officer_of_club
 
 from django.contrib import messages
 from django.contrib.auth import login
@@ -91,3 +91,39 @@ def add_tournament_organiser_list(request, tournament_id):
         messages.error(request, "You are not a member of the club that organises this tournament, you can view the basic tournament details from the club's page.")
         return redirect('show_club', club_id=club.id)
     return redirect('show_tournament', tournament_id=tournament.id)
+
+@login_required
+@tournament_exists
+def add_organiser_to_tournament(request, tournament_id, member_id):
+    """Allow the head organiser of a tournament to assign other officers/owner of the club organising the tournament to officer."""
+
+    if not Membership.objects.filter(id=member_id).exists(): #View function takes various arguments, so decorator to check for it throws an error
+        messages.error(request, 'No membership with id ' + str(member_id) + ' exists.')
+        return redirect(settings.REDIRECT_URL_WHEN_LOGGED_IN)
+    else:
+
+        tournament = Tournament.objects.get(id = tournament_id)
+        new_organiser_member = Membership.objects.get(id = member_id)
+
+        if is_lead_organiser_of_tournament(request.user, tournament):
+            if not is_user_organiser_of_tournament(new_organiser_member.user, tournament):
+                if not is_participant_in_tournament(new_organiser_member.user, tournament):
+                    if is_user_owner_of_club(new_organiser_member.user, tournament.club) or is_user_officer_of_club(new_organiser_member.user, tournament.club):
+                        Organiser.objects.create(
+                            member = new_organiser_member,
+                            tournament = tournament
+                        )
+                        messages.success(request, '@' + new_organiser_member.user.username + ' is now an organiser of the tournament: ' + tournament.name + ".")
+                    else: #Access denied organiser can only assign organiser roles to other members who are officers or the owner
+                        messages.error(request, 'You can only assign officers or the owner to be organisers for tournaments.')
+                else: # User is a participant, so they cannot be an organiser of the same tournament
+                    messages.warning(request, '@' + new_organiser_member.user.username + ' is already a participant of tournament ' + tournament.name)
+            else: # User is already an organiser
+                if is_user_organiser_of_tournament(new_organiser_member.user, tournament):
+                    messages.error(request, "You are the lead organiser. You cannot add yourself as organiser.")
+                else:
+                    messages.warning(request, '@' + new_organiser_member.user.username + ' is already an organiser of tournament ' + tournament.name)
+        else: # Access denied, member isn't the lead organiser of tournament
+            messages.warning(request, 'Only the lead organiser can assign other organisers to their tournament.')
+
+        return redirect('show_tournament', tournament_id=tournament.id)
